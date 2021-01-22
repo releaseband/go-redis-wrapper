4b973b8ccc87b097f8c67afc951c6183e982ce38
Ayupov Ayaz <ayaz@releaseband.com>
2021-01-22 11:55:59 +0300

    remove deprecated funcs

--- redis/metrics_decorator.go
+++ redis/metrics_decorator.go
@@ -121,18 +121,6 @@ func (r RedisMetricsDecorator) Entity() string {
 	return r.client.Entity()
 }
 
-func (r RedisMetricsDecorator) HGetAll(ctx context.Context, key string) (map[string]string, error) {
-	return r.client.HGetAll(ctx, key)
-}
-
-func (r RedisMetricsDecorator) HSet(ctx context.Context, key string, val ...interface{}) error {
-	return r.client.HSet(ctx, key, val)
-}
-
-func (r RedisMetricsDecorator) HDel(ctx context.Context, key string, fields ...string) error {
-	return r.client.HDel(ctx, key, fields...)
-}
-
 func (r RedisMetricsDecorator) SlotsCount(ctx context.Context) (int, error) {
 	return r.client.SlotsCount(ctx)
 }
